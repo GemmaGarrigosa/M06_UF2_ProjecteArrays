769c35f4814cd901f1743f50a9006727bb6cb9ed
Gemma <gemma.garrigosa@gmail.com>
2023-12-18 18:14:30 +0100

    Afegit el que vaig fer el dijous

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
+/* PART 0: Accedir al JSON i començar a llistar dades */
 
-let info = []
+let info = [];
+let pokemons=[];
 // POKEMONS
 fetch("js/data/pokemon.json")
     .then((response) => response.json())
     .then((data) => {
         let nomsPokemon = data.pokemon;
+        console.log(`Resultat del data.pokemon: ${JSON.stringify(data.pokemon)}`);
         nomsPokemon.forEach((pokemon) => {
+            pokemons.push([pokemon.name,pokemon.img,pokemon.weight]);
             info.push({ pokemon: pokemon.name, municipi: " ", meteorit: " ", movie: ""  }); 
+            
         });
     });
 
@@ -50,10 +55,60 @@ fetch("js/data/movies.json")
                 info[index] = { pokemon: " ", municipi: " ", meteorit: " ", movie: movie.title }; 
             }
         });
-
+        
         // Mostra les dades en una taula
         console.table(info);
     });
 
 
+/* PART 1 Funcions i arrays */
 
+
+function iniciar(){
+	// location.reload();
+    printList();
+}
+
+// function orderList(ordre){
+// 	if (ASC){
+// 		pokemons.sort();
+// 	}else {
+// 		pokemons.reverse();
+// 	}
+// }
+
+// function searchList(){
+// 	//torna posicio donat un prompt0
+// }
+
+// function calcMitjana(){
+//     //fer servir toFixed()
+// }
+
+function printList(){
+    
+   let div = document.getElementById("taulaPokemons");
+   let taula = "<table>";
+//     for (let i = 0; i < pokemons.length; i++){
+//         taula += "<tr>";
+//         for (let j = 0; j < pokemons.length; j++){
+//             taula += `<td> ${pokemons[i][j]}</td>`;
+            
+//         }
+//         taula += "</tr>";
+//     }
+//     taula += "</table>";
+//     div.innerHTML = taula;
+        taula += "<th>#</th><th>Imatge</th><th>Nom</th><th>Pes</th>";
+        pokemons.forEach((pokemon,index) => {
+            let nomPokemon = pokemons[index][0];
+            let imatgePokemon = pokemons[index][1];
+            let pesPokemon = pokemons[index][2];
+            taula+= "<tr>";
+            taula += `<td> ${index} </td> <td><img src="${imatgePokemon}"><td> ${nomPokemon} </td></td><td>${pesPokemon}</td>`;
+            taula += "</tr>";
+        });
+        
+        taula += "</table>";
+        div.innerHTML = taula;
+}
